22142cb212c7654c0b116ee0ca0403a686784b34
joyytoo <joytoo97@gmail.com>
2023-04-02 21:44:10 +0300

    Set up rooms and users

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
+
+  resources :rooms
+  resources :users
+  root 'rooms#index'
 end
